9c29e70cb3a972b72127f1f2a5d56cbbeef9b791
Nenad Livaic <nlivaic@erecruit.com>
2019-09-22 22:13:35 +0200

    Initial commit

---
+++ ExpressionRewriter/ExpressionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ExpressionRewriter
+{
+	public static class ExpressionExtensions
+	{
+		public static R Call<T, R>(this Expression<Func<T, R>> expr, T param1)
+		{
+			throw new InvalidOperationException("Do not call this method directly.");
+		}
+		public static U Call<T, R, U>(this Expression<Func<T, R, U>> expr, T param1, R param2)
+		{
+			throw new InvalidOperationException("Do not call this method directly.");
+		}
+	}
+}
---
+++ ExpressionRewriter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpressionRewriter
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
